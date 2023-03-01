9f1c91ee8b479ee43fcd791a619084de90247c33
Samuel Luiz <97702597+samluiz@users.noreply.github.com>
2023-03-01 00:16:23 -0300

    remove scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <ThemeContextProvider>
         <TailwindUpdater>
-          <div className="w-screen relative h-screen grid place-items-center grid-flow-row dark:bg-gray-700 bg-gray-200 duration-500 overflow-x-scroll overflow-y-hidden">
+          <div className="w-screen relative h-screen grid place-items-center grid-flow-row dark:bg-gray-700 bg-gray-200 duration-500 overflow-y-scroll overflow-x-hidden">
             <MainContainer />
           </div>
         </TailwindUpdater>
