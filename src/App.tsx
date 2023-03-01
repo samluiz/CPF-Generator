7d1a72eb3416627461cea31223c414b9f88b3ceb
Samuel Luiz <97702597+samluiz@users.noreply.github.com>
2023-03-01 00:17:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <ThemeContextProvider>
         <TailwindUpdater>
-          <div className="w-screen relative h-screen grid place-items-center grid-flow-row dark:bg-gray-700 bg-gray-200 duration-500 overflow-y-scroll overflow-x-hidden">
+          <div className="w-screen relative h-screen grid place-items-center grid-flow-row dark:bg-gray-700 bg-gray-200 duration-500 overflow-y-auto overflow-x-hidden">
             <MainContainer />
           </div>
         </TailwindUpdater>
